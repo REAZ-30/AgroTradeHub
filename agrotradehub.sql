b47e7b1386eab64e1b153f4f0c75f010729f9236
Abir86053 <abir86053@gmail.com>
2025-12-13 23:00:23 +0600

    Updated database

--- agrotradehub.sql
+++ agrotradehub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2025 at 07:33 AM
+-- Generation Time: Dec 13, 2025 at 05:59 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -76,7 +76,12 @@ INSERT INTO `orders` (`id`, `customer_id`, `total_amount`, `status`, `order_date
 (26, 21, 880.00, 'completed', '2025-12-12 11:59:03', 'Adabor', '2025-12-12 11:59:03', 'ORD-20251212-125903-1680', 'bkash', 'paid', 'TRX123456789', '01617063610', '01617063610'),
 (27, 21, 60.00, 'completed', '2025-12-12 12:32:21', 'Adabor', '2025-12-12 12:32:21', 'ORD-20251212-133221-4912', 'bkash', 'paid', 'TRX123456789', '01786335550', '01617063610'),
 (28, 22, 220.00, 'completed', '2025-12-12 13:37:10', 'Mohammadpur', '2025-12-12 13:37:10', 'ORD-20251212-143710-4847', 'bkash', 'paid', 'TRX123456789', '01234323131', '01403161496'),
-(29, 22, 110.00, 'completed', '2025-12-12 13:37:46', 'Mohammadpur', '2025-12-12 13:37:46', 'ORD-20251212-143746-8393', 'bkash', 'paid', 'TRX123456789', '01234323131', '01403161496');
+(29, 22, 110.00, 'completed', '2025-12-12 13:37:46', 'Mohammadpur', '2025-12-12 13:37:46', 'ORD-20251212-143746-8393', 'bkash', 'paid', 'TRX123456789', '01234323131', '01403161496'),
+(30, 20, 110.00, 'completed', '2025-12-13 07:18:00', 'Mohammadpur', '2025-12-13 07:18:00', 'ORD-20251213-081800-8698', 'bkash', 'paid', 'TRX123456789', '01403161496', '01403161496'),
+(31, 20, 962.50, 'pending', '2025-12-13 09:33:26', 'Mohammadpur', '2025-12-13 09:33:26', 'ORD-20251213-103326-7501', 'bkash', 'paid', 'TRX1777', '017', '01403161496'),
+(32, 20, 32.50, 'pending', '2025-12-13 09:49:59', 'Mohammadpur', '2025-12-13 09:49:59', 'ORD-20251213-104959-2936', 'bkash', 'paid', 'TRX22222', '017', '01403161496'),
+(33, 26, 32.50, 'pending', '2025-12-13 16:43:50', 'Dhanmondi', '2025-12-13 16:43:50', 'ORD-20251213-174350-5501', 'bkash', 'paid', 'TRX365353', '01776', '017112227'),
+(34, 26, 88.00, 'pending', '2025-12-13 16:44:58', 'Dhanmondi', '2025-12-13 16:44:58', 'ORD-20251213-174458-4943', 'bkash', 'paid', 'TRX534335', '01776', '017112227');
 
 -- --------------------------------------------------------
 
@@ -101,7 +106,14 @@ INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`, 
 (21, 26, 9, 1, 800.00, 23),
 (22, 27, 10, 1, 50.00, 23),
 (23, 28, 14, 2, 100.00, 24),
-(24, 29, 14, 1, 100.00, 24);
+(24, 29, 14, 1, 100.00, 24),
+(25, 30, 14, 1, 100.00, 24),
+(26, 31, 13, 1, 25.00, 24),
+(27, 31, 10, 1, 50.00, 23),
+(28, 31, 9, 1, 800.00, 23),
+(29, 32, 13, 1, 25.00, 24),
+(30, 33, 13, 1, 25.00, 24),
+(31, 34, 12, 1, 80.00, 23);
 
 -- --------------------------------------------------------
 
@@ -134,7 +146,8 @@ INSERT INTO `products` (`id`, `seller_id`, `category_id`, `name`, `description`,
 (11, 23, 4, 'Hilsha fish', 'Fresh Hilsha Fish', 1500.00, 4, 'https://png.pngtree.com/png-vector/20250221/ourlarge/pngtree-hilsha-fish-png-image_15552378.png', 'fish', 1, '2025-12-08 05:54:43'),
 (12, 23, 6, 'Milk', 'Fresh milk', 80.00, 20, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTVNJXzJm560I5GjhY8Cgedrs79dwjPo565w&s', 'dairy', 1, '2025-12-08 05:55:55'),
 (13, 24, 1, 'Potato', 'Fresh potato', 25.00, 14, 'https://png.pngtree.com/png-vector/20240615/ourlarge/pngtree-potatoes-image-png-image_12749734.png', 'vegetable', 1, '2025-12-08 05:57:14'),
-(14, 24, 3, 'Mango', 'Fresh mango', 100.00, 3, 'https://static.vecteezy.com/system/resources/previews/026/795/004/non_2x/mango-fruit-tropical-transparent-png.png', 'fruit', 1, '2025-12-08 05:58:32');
+(14, 24, 3, 'Mango', 'Fresh mango', 100.00, 2, 'https://static.vecteezy.com/system/resources/previews/026/795/004/non_2x/mango-fruit-tropical-transparent-png.png', 'fruit', 1, '2025-12-08 05:58:32'),
+(15, 28, 3, 'Stawbery', 'Farm Fresh Products from farmers', 230.00, 15, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgbSM5DX2tlzSf0khV37VpQsM9g06A-7F4Uw&s', 'fruit', 1, '2025-12-13 16:50:59');
 
 -- --------------------------------------------------------
 
@@ -156,7 +169,8 @@ CREATE TABLE `reviews` (
 --
 
 INSERT INTO `reviews` (`id`, `product_id`, `customer_id`, `rating`, `comment`, `created_at`) VALUES
-(3, 9, 21, 4, 'BEST BEEEF EVER', '2025-12-12 12:00:13');
+(3, 9, 21, 4, 'BEST BEEEF EVER', '2025-12-12 12:00:13'),
+(4, 14, 20, 3, 'thanks zawad bin', '2025-12-13 07:20:07');
 
 -- --------------------------------------------------------
 
@@ -187,7 +201,10 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `user_type`, `full_n
 (22, 'nakhil1', 'nakhil@gmail.com', '123456', 'customer', 'Nakhil Jahan Akanda', '01403161496', 'Mohammadpur', '2025-12-08 05:37:57', 0),
 (23, 'nafis1', 'nafis@gmail.com', '123456', 'seller', 'Nafis Ahmed', '01617063610', 'Azimpur', '2025-12-08 05:39:41', 0),
 (24, 'zawad1', 'zawad@gmail.com', '123456', 'seller', 'Zawad Bin', '01617063610', 'Mohammadpur', '2025-12-08 05:40:35', 0),
-(25, 'admin', 'admin@gmail.com', '123456', 'admin', 'Real Admin', NULL, NULL, '2025-11-22 09:00:42', 0);
+(25, 'admin', 'admin@gmail.com', '123456', 'admin', 'Real Admin', NULL, NULL, '2025-11-22 09:00:42', 0),
+(26, 'tonmoy1', 'tonmoy@gmai.com', '123456', 'customer', 'Tonmoy', '017112227', 'Dhanmondi', '2025-12-13 16:32:27', 0),
+(27, 'sagor1', 'sagor@gmail.com', '123456', 'customer', 'Sagor', '016', 'Bosila', '2025-12-13 16:46:04', 0),
+(28, 'jmam1', 'jimam@gmail.com', '123456', 'seller', 'Jimam', '017', 'Rayerbazar', '2025-12-13 16:47:37', 0);
 
 --
 -- Indexes for dumped tables
@@ -254,31 +271,31 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Constraints for dumped tables
