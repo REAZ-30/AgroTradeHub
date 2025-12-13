f080e1c54242278729d5e82efe4c9b8d2326087f
Nakhil-Jahan-Akanda <brianwils100@gmail.com>
2025-12-13 12:20:42 +0600

    Add files via upload

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "agrotradehub";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            // Don't show error message, just return null
+            return null;
+        }
+        return $this->conn;
+    }
+}
+?>
